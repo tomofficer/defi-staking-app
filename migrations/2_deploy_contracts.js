5c29497d688809bbe5311e80d05a62c1c104c667
Tom Officer <thomasofficerjr@gmail.com>
2022-02-21 22:45:25 -0800

    updated contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
 const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
 module.exports = async function(deployer) {
+  //deploy Mock Tether Contract
   await deployer.deploy(Tether);
+
+  //deploy RWD Contract
+  await deployer.deploy(RWD);
+
+  //deploy DecentralBank Contract
+  await deployer.deploy(DecentralBank);
 };
