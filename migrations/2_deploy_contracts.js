f56f2a48ad27290b85d16f38e39e8eaae9f23b25
Tom Officer <thomasofficerjr@gmail.com>
2022-03-03 21:42:49 -0800

    added mocha testing for smart contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,13 +2,22 @@ const Tether = artifacts.require('Tether');
 const RWD = artifacts.require('RWD');
 const DecentralBank = artifacts.require('DecentralBank');
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
   //deploy Mock Tether Contract
   await deployer.deploy(Tether);
+  const tether = await Tether.deployed();
 
   //deploy RWD Contract
   await deployer.deploy(RWD);
+  const rwd = await RWD.deployed();
 
   //deploy DecentralBank Contract
-  await deployer.deploy(DecentralBank);
+  await deployer.deploy(DecentralBank, rwd.address, tether.address);
+  const decentralBank = await DecentralBank.deployed();
+
+  //transfer all RWD tokens to Decentral bank
+  await rwd.transfer(decentralBank.address, '1000000000000000000000000');
+
+  //distribute 100 tether tokens to investor
+  await tether.transfer(accounts[1], '100000000000000000000');
 };
