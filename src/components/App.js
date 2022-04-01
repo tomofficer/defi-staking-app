b434e80a86c9161f3cddcc661f36b7f5b48495e0
Tom Officer <thomasofficerjr@gmail.com>
2022-04-01 00:15:10 -0700

    started building frontend

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './Navbar';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: '0x0',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar account={this.state.account} />
+        <div className='text-center'>
+          <h1></h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import bank from '../bank.png';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav
+        className='navbar navbar-dark fixed-top shadow p-0'
+        style={{ backgroundColor: 'black', height: '50px' }}>
+        <a
+          className='navbar-brand col-sm-3 col-md-2 mr-0'
+          style={{ color: 'white' }}>
+          <img
+            src={bank}
+            width='50'
+            height='30'
+            className='d-inline-block align-top'
+            alt='bank image'
+            style={{ marginRight: '10px', borderRadius: '30px' }}
+          />
+          DAPP Yield Staking (Decentralized Banking)
+        </a>
+        <ul className='navbar-nav px-3'>
+          <li className='text-no-wrap d-none nav-item d-sm-none d-sm-block'>
+            <small style={{ color: 'white' }}>
+              ACCOUNT NUMBER: {this.props.account}
+            </small>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
